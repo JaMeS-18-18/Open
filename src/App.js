b17486669e786e06d1a3cab70ddceff83e592a9b
JaMeS-18-18 <chitak1818@gmail.com>
2023-03-30 03:24:34 +0500

    update lang

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import boy from './assets/boy.png'
 import opa from './assets/opa.png'
 import telefon from './assets/telefon.png'
 import xarita from './assets/xarita.png'
-import contact_bg from './assets/contact_bg.png'
 import oka from './assets/oka.png'
 import opa2 from './assets/opa2.png'
 import { useTranslation } from 'react-i18next'
@@ -69,26 +68,24 @@ function App() {
           <div className='row'>
             <div className='sect_1  my-3 ps-3'>
               <span>
-                <span className='me-3'>-</span> Savdo va xizmat ko'rsatish korxonalari uchun
-              </span>
+                <span className='me-3'>-</span>{t("H_3")} </span>
             </div>
 
             <div className='sect_2  my-3 justify-content-center justify-content-md-end '>
               <div className='for_width'>
                 <span className='first  fs-md-1'>
-                  OPEN <span className='text-dark'> muddatli kartasi - </span>
-                  Bu biznesingiz o’sishi va rivojlanishi
+                  OPEN <span className='text-dark'> {t("H_4")} - </span>
+                  {t("H_5")}
                 </span>
                 <div className='my-3 text-dark'>
-                  Biz bilan hamkorlik qilish daromadingizni oshiradi va bozordagi raqobatdosh mavqeingizni mustahkamlaydi.
-                  Open mijozlarga qimmatroq xaridlar uchun pul beradi
+                {t("H_6")}
                 </div>
               </div>
             </div>
 
             <div className='sect_1  my-3'>
               <span className='text-dark'>
-                <span className='sect_1 me-3'>01</span> O'rtacha chekning va foydangizning o’sishi
+                <span className='sect_1 me-3'>01</span> {t("H_7")}
               </span>
             </div>
           </div>
@@ -104,26 +101,27 @@ function App() {
                 <div className=' col-6'>
                   <div className='left_col6_card'>
                     <div>
-                      <div className='d-flex'><p className='me-3 text-secondary'>Muddatli to’lovsiz</p> <p>A muzlatgichi</p></div>
-                      <div className='d-flex'><p className='me-3 text-secondary'>Umumiy qiymati</p> <p>5 mln So’m</p></div>
+                      <div className='d-flex'><p className='me-3 text-secondary'>{t("H_8")}</p> <p>A {t("H_9")}</p></div>
+                      <div className='d-flex'><p className='me-3 text-secondary'>{t("H_10")}</p> <p>5 mln So’m</p></div>
                     </div>
 
                     <div className='set2_left'>
-                      <div className='d-flex align-items-center'><p className='sect2_left_static'>Foyda 20%</p> <p className='d-none d-sm-flex'>1 mln So’m</p></div>
-                      <div className='d-flex align-items-center'><p className='sect2_left_static2'>Tannarx</p> <p className='d-none d-sm-flex'>4 mln So’m</p></div>
+                      <div className='d-flex align-items-center'><p className='sect2_left_static'>{t("H_11")} 20%</p> <p className='d-none d-sm-flex'>1 mln So’m</p></div>
+                      <div className='d-flex align-items-center'><p className='sect2_left_static2'>{t("H_13")}</p> <p className='d-none d-sm-flex'>4 mln So’m</p></div>
                     </div>
                   </div>
                 </div>
                 <div className=' col-6'>
                   <div className='left_col6_card'>
                     <div>
-                      <div className='d-flex'><p className='me-3 text-secondary'>Muddatli to’lovsiz</p> <p>A muzlatgichi</p></div>
-                      <div className='d-flex'><p className='me-3 text-secondary'>Umumiy qiymati</p> <p>5 mln So’m</p></div>
+                      <div className='d-flex'><p className='me-3 text-secondary'>{t("H_8")}</p> <p>B {t("H_9")}</p></div>
+                      <div className='d-flex'><p className='me-3 text-secondary'>{t("H_10")}</p> <p>9 mln So’m</p></div>
                     </div>
 
                     <div className='set2_left2'>
-                      <div className='d-flex align-items-center'><p className='sect2_left_statics'>Foyda 20%</p> <p className='d-none d-sm-flex'>1 mln So’m</p></div>
-                      <div className='d-flex align-items-center'><p className='sect2_left_static3'>Tannarx</p> <p className='d-none d-sm-flex'>4 mln So’m</p></div>
+                      <div className='d-flex align-items-center'><p className='sect2_left_statics'>{t("H_11")} 22%</p> <p className='d-none d-sm-flex'>1 mln So’m</p></div>
+                      <div className='d-flex align-items-center'><p className='sect2_left_statics2'>{t("H_12")}</p> <p className='d-none d-sm-flex'>0.5 mln So’m</p></div>
+                      <div className='d-flex align-items-center'><p className='sect2_left_static3'>{t("H_13")}</p> <p className='d-none d-sm-flex'>4 mln So’m</p></div>
                     </div>
                   </div>
                 </div>
@@ -143,29 +141,27 @@ function App() {
           <div className='row'>
             <div className='col-md-6 sect3_left'>
               <div className='sect_1 my-5 h-25'>
-                <span className='text-dark'>
-                  <span className='sect_1 me-3'>02</span> Yangi mijozlar va xaridlar sonining oshishi
+                <span className='text-dark fs-4'>
+                  <span className='sect_1 me-3'>02</span> {t("H_14")}
                 </span>
               </div>
 
-              <div className='sect_1 my-5 h-25'>
+              <div className='sect_1 my-5 h-25 fs-4'>
                 <span className='text-dark'>
-                  <span className='sect_1 me-3'>03</span> Xavfning yo’qligi
+                  <span className='sect_1 me-3'>03</span> {t("H_15")}
                 </span>
               </div>
 
-              <div className='sect_1 my-5 h-25'>
+              <div className='sect_1 my-5 h-25 fs-4'>
                 <span className='text-dark'>
-                  <span className='sect_1 me-3'>04</span> Tezkor hisob-kitoblar
+                  <span className='sect_1 me-3'>04</span> {t("H_17")}
                 </span>
               </div>
             </div>
             <div className='col-md-6 sect3_right'>
-              <p className='my-5 h-25'>
-                Siz faqat Open kartasi orqali amalga oshirilgan xaridlar uchun komissiya to'laysiz. Biz barcha o'zaro hisob-kitoblarni o'zimiz tashkil qilamiz va soliq masalalari bo'yicha mutaxassislaringizga maslahat beramiz
-              </p>
-              <p className='my-5 h-25'>
-                Siz mijozning xaridi uchun to’lovni darhol qabul qilasiz. So’ngra, muddatli to’lov pulini mijoz bankka to’laydi
+              <p className='my-5 h-25 fs-5'>{t("H_16")}</p>
+              <p className='my-5 h-25 fs-5' >
+              {t("H_18")}
               </p>
             </div>
           </div>
@@ -192,15 +188,14 @@ function App() {
             <div className='col-12 col-md-6'>
               <div className='sect_1 my-5 h-25'>
                 <span className='text-dark'>
-                  <span className='sect_1 me-3'>05</span> Sheriklik marketingi
+                  <span className='sect_1 me-3'>05</span> {t("H_19")}
                 </span>
               </div>
             </div>
             <div className='col-12 col-md-6'>
               <div className='sect_1 fs-5 my-5 h-25'>
                 <span className='text-dark'>
-                  Biz reklamalarda o’z mijozlarimiz haqida gapiramiz, dasturimiz va saytimizda ular haqida ma’lumot ko’rsatamiz
-                  Savdolaringizni rag’batlantirish maqsadida turli aksiyalar o’tkazamiz
+                {t("H_20")}
                 </span>
               </div>
             </div>
@@ -208,15 +203,14 @@ function App() {
             <div className='col-12 col-md-6'>
               <div className='sect_1 my-5 h-25'>
                 <span className='text-dark'>
-                  <span className='sect_1 me-3'>06</span> Sheriklik marketingi
+                  <span className='sect_1 me-3'>06</span> {t("H_21")}
                 </span>
               </div>
             </div>
             <div className='col-12 col-md-6'>
               <div className='sect_1 fs-5 my-5 h-25'>
                 <span className='text-dark'>
-                  Biz reklamalarda o’z mijozlarimiz haqida gapiramiz, dasturimiz va saytimizda ular haqida ma’lumot ko’rsatamiz
-                  Savdolaringizni rag’batlantirish maqsadida turli aksiyalar o’tkazamiz
+                {t("H_22")}
                 </span>
               </div>
             </div>
@@ -224,15 +218,14 @@ function App() {
             <div className='col-12 col-md-6'>
               <div className='sect_1 my-5 h-25'>
                 <span className='text-dark'>
-                  <span className='sect_1 me-3'>07</span> Sheriklik marketingi
+                  <span className='sect_1 me-3'>07</span> {t("H_23")}
                 </span>
               </div>
             </div>
             <div className='col-12 col-md-6'>
               <div className='sect_1 fs-5 my-5 h-25'>
                 <span className='text-dark'>
-                  Biz reklamalarda o’z mijozlarimiz haqida gapiramiz, dasturimiz va saytimizda ular haqida ma’lumot ko’rsatamiz
-                  Savdolaringizni rag’batlantirish maqsadida turli aksiyalar o’tkazamiz
+                {t("H_24")}
                 </span>
               </div>
             </div>
@@ -244,27 +237,26 @@ function App() {
               <div className='d-flex'>
                 <div className='rectangle me-3 mt-1'>
                 </div>
-                <p>Hamkorlar mavjud viloyatlar</p>
+                <p>{t("H_25")}</p>
               </div>
               <div className='d-flex'>
                 <div className='rectangle2 me-3 mt-1'>
                 </div>
-                <p>Qamrab olish rejalashtirilayotgan viloyatlar</p>
+                <p>{t("H_26")}</p>
               </div>
             </div>
 
             <div className='col-12 col-md-6'>
               <div className='sect_1 my-5 h-25'>
                 <span className='text-dark'>
-                  <span className='sect_1 me-3'>08</span> Sheriklik marketingi
+                  <span className='sect_1 me-3'>08</span> {t("H_27")}
                 </span>
               </div>
             </div>
             <div className='col-12 col-md-6'>
               <div className='sect_1 fs-5 my-5 h-25'>
                 <span className='text-dark'>
-                  Biz reklamalarda o’z mijozlarimiz haqida gapiramiz, dasturimiz va saytimizda ular haqida ma’lumot ko’rsatamiz
-                  Savdolaringizni rag’batlantirish maqsadida turli aksiyalar o’tkazamiz
+                {t("H_28")}
                 </span>
               </div>
             </div>
@@ -279,13 +271,13 @@ function App() {
           <div className='contact_card'>
             <div className='row'>
               <div className=' col-12 col-sm-6'>
-                <h5 className='my-4 fs-3'>Hamkorga aylanish</h5>
-                <p className='fs-6'>Ushbu shaklni to’ldiring, va biz sizga muddatli <br /> to’lov taqdimotini o’tkazamiz va shartnoma <br /> tuzishga yordam beramiz</p>
+                <h5 className='my-4 fs-3'>{t("H_29")}</h5>
+                <p className='fs-6'>{t("H_30")}</p>
               </div>
               <div className=' col-12 col-sm-6'>
-                <input className='input-group my-3 p-2' type="text" placeholder='Tashkilot nomi' />
-                <input className='input-group my-3 p-2' type="text" placeholder='Tashkilot nomi' />
-                <button className='btn btn-outline button input-group my-4 p-3'>Hamkorga aylanish</button>
+                <input className='input-group my-3 p-2' type="text" placeholder={t("H_32")} />
+                <input className='input-group my-3 p-2' type="text" placeholder={t("H_32")} />
+                <button className='btn btn-outline button input-group my-4 p-3'>{t("H_29")}</button>
               </div>
             </div>
           </div>
@@ -298,7 +290,7 @@ function App() {
           <div className='row'>
             <div className='sect_1 col-12 my-3'>
               <span>
-                <span className='me-3'>-</span> Savdo va xizmat ko'rsatish korxonalari uchun
+                <span className='me-3'>-</span>  {t("H_33")}
               </span>
             </div>
 
@@ -307,12 +299,12 @@ function App() {
             <div className='sect_2 col-md-8 my-3  justify-content-center '>
               <div className='foot_fs'>
                 <span className='first  fs-md-1'>
-                  <span className='text-dark'>  Biz karta </span>biznesini rivojlantirish
-                  <span className='text-dark'>  uchun yangi </span> imkoniyatlar <span className='text-dark'>
-                    taqdim etamiz.</span>
+                  <span className='text-dark'>  {t("H_43")} </span>{t("H_44")}
+                  <span className='text-dark'>  {t("H_45")} </span> {t("H_46")} <span className='text-dark'>
+                  {t("H_47")}.</span>
                 </span>
                 <div className='my-3 text-dark w-100'>
-                  Hamkorlik tarmog’imiz va OpenInstallment texnologiyalari asosida kobrendli muddatli to’lov kartasini atigi bir necha xafta ichida ishga tushirish mumkin. Agarda sizda shaxsiy muddatli to’lov tizimingiz yoki marketplace’ingiz bo’lsa, siz OpenInstallment yordamida o’zingizning xizmatingizni va raqobatchi pozitsiyangizni mustahkamlashingiz mumkin
+                {t("H_35")}
                 </div>
               </div>
             </div>
@@ -323,22 +315,20 @@ function App() {
             <div className='col-md-6 d-none d-md-block'>
               <div className='purple_card'>
                 <p>01</p>
-                <p className='fs-5'>Faol karta mijozlari sonining oshishi
-                  Open mijozlarining hisob-kitob hisobvaraqlari sizning bankingizda ochiladi va yuritiladi</p>
+                <p className='fs-5'>{t("H_36")}</p>
               </div>
             </div>
 
             <div className='col-md-4'>
               <div className='purple_card'>
                 <p>02</p>
-                <p className='fs-5'>Faol karta mijozlari sonining oshishi
-                  Open mijozlarining hisob-kitob hisobvaraqlari sizning bankingizda ochiladi va yuritiladi</p>
+                <p className='fs-5'>{t("H_37")}</p>
               </div>
             </div>
             <div className='col-md-8'>
               <div className='gray_card'>
                 <p>03</p>
-                <p className='fs-5'>Yuqori daromad Open kartasi orqali to’lov qilinganda bank up-front kommisiyasiga ega bo’ladi, bu esa oz navbatida 34% gacha NPV’ni ta’minlaydi</p>
+                <p className='fs-5'>{t("H_38")}</p>
               </div>
             </div>
 
@@ -346,7 +336,7 @@ function App() {
             <div className='col-md-4'>
               <div className='gray_card'>
                 <p>04</p>
-                <p className='fs-5'>Xavflar pastroq Open kartasi orqali naqd pul yechish bloklangan. Belgilangan limitni faqat hamkor do’konlarda sarflash mumkin</p>
+                <p className='fs-5'>{t("H_39")}</p>
               </div>
 
             </div>
@@ -360,14 +350,13 @@ function App() {
             <div className='col-md-6'>
               <div className='gray_card2'>
                 <p>05</p>
-                <p className='fs-5'>Yuqori daromad Open kartasi orqali to’lov qilinganda bank up-front kommisiyasiga ega bo’ladi, bu esa oz navbatida 34% gacha NPV’ni ta’minlaydi</p>
+                <p className='fs-5'>{t("H_40")}</p>
               </div>
             </div>
             <div className='col-md-6 h-100'>
               <div className='purple_card2'>
                 <p>06</p>
-                <p className='fs-5'>Faol karta mijozlari sonining oshishi
-                  Open mijozlarining hisob-kitob hisobvaraqlari sizning bankingizda ochiladi va yuritiladi</p>
+                <p className='fs-5'>{t("H_41")}</p>
               </div>
             </div>
 
@@ -379,7 +368,7 @@ function App() {
         <div className='row'>
           <div className='col-12 d-flex justify-content-center align-items-center'>
             <div className='foot_info'>
-              <p className='fs-5 text-white'>Hamkorlikni muhokama qilish</p>
+              <p className='fs-5 text-white'>{t("H_42")}</p>
               <p className='fs-5 text-white'> <i class="bi bi-envelope me-3"></i>hello@opentech.uz</p>
               <p className='fs-5 text-white'><i class="bi bi-telephone me-2"></i> +998 (90) 968-7881</p>
             </div>
